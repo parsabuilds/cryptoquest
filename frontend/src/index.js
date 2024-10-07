ed29375f9cc71d9ccdbe5f78c04caac051bf72b6
elonmusk <parsarajabi7@gmail.com>
2024-10-07 15:51:35 -0400

    Remove 'frontend' submodule and add as regular directory

---
+++ frontend/src/index.js
@@ -0,0 +1,19 @@
+// src/index.js
+
+import React from 'react';
+import ReactDOM from 'react-dom/client'; // Note the '/client' import
+import App from './App';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import theme from './theme';
+
+const root = ReactDOM.createRoot(document.getElementById('root')); // Create root
+
+root.render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
